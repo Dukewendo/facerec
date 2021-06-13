a4ade3330adec0332aaf29125d44c317c1030a76
Dukewendo <dommct@yahoo.co.uk>
2021-06-13 15:51:53 +0100

    psql update

--- faceRec-api/server.js
+++ faceRec-api/server.js
@@ -18,8 +18,10 @@ const db = knex({
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true,
-  }
+    ssl: {
+      rejectUnauthorized: false,
+  },
+},
 });
 
 app.use(bodyParser.json());
