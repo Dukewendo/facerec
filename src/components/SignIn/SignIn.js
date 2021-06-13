a4d8dbd4dabfe1ca65225545b533eadd5bf0847e
Dukewendo <dommct@yahoo.co.uk>
2021-06-13 17:09:38 +0100

    server-update

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
   }
      
   onSubmitSignIn = () => {
-  fetch("https://stark-beach-12250.herokuapp.com/Signin", {
+  fetch("https://stark-beach-12250.herokuapp.com/signin", {
     method: "post",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify({
