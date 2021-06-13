33bd76299ba7004bfe0d87f23fe98ebf24ae0839
Dukewendo <dommct@yahoo.co.uk>
2021-06-13 16:27:32 +0100

    remote db

--- faceRec-api/server.js
+++ faceRec-api/server.js
@@ -2,45 +2,48 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { json } = require("body-parser");
 const bcrypt = require("bcrypt");
-const saltRounds = 10;
 const app = express();
 const cors = require("cors");
 const knex = require("knex");
-const { unstable_createPortal } = require("react-dom");
-const register = require('./controllers/register');
-const signin = require ('./controllers/signin');
-const profile = require ('./controllers/profile');
-const image = require ('./controllers/image');
-const { Profiler } = require("react");
-
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const db = knex({
-  client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
+    },
   },
-},
 });
 
 app.use(bodyParser.json());
 app.use(cors());
 
 //signin --> POST (request user info) = success/fail
-app.post("/signin", (req, resp) => {signin.handleSignin(req, resp, db, bcrypt)});
+app.post("/signin", (req, resp) => {
+  signin.handleSignin(req, resp, db, bcrypt);
+});
 
 //register --> POST = new user object returned
-app.post("/register", (req, resp) => {register.handleRegister(req, resp, db, bcrypt)});
+app.post("/register", (req, resp) => {
+  register.handleRegister(req, resp, db, bcrypt);
+});
 
 //profile/:userId --> GET = returns user
-app.get("/profile/:id", (req, resp) => { profile.handleProfileGet(req, resp, db)});
+app.get("/profile/:id", (req, resp) => {
+  profile.handleProfileGet(req, resp, db);
+});
 
 //image end point --> PUT --> returns updated count for user
-app.put("/image", (req, resp) => {image.handleImage(req, resp, db)});
-app.post("/imageurl", (req, resp) => {image.handleApiCall(req, resp)});
-
-
+app.put("/image", (req, resp) => {
+  image.handleImage(req, resp, db);
+});
+app.post("/imageurl", (req, resp) => {
+  image.handleApiCall(req, resp);
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`app started- port ${process.env.PORT}`);
